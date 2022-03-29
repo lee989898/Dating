a6061fd3c395989c9642d9713de3ea5a6a1348aa
lee989898 <wnsrud1352@gmail.com>
2022-03-29 21:44:37 +0900

    feat: 스플래쉬, 인트로 디자인

---
+++ app/src/main/java/com/lee989898/dating/auth/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.lee989898.dating.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lee989898.dating.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
