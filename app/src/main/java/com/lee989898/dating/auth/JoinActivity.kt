e0ae081795df5563d501986eaf5d94fa12261527
lee989898 <wnsrud1352@gmail.com>
2022-03-30 00:16:15 +0900

    feat: 파이어베이스 연결 및 회원가입 구현

--- app/src/main/java/com/lee989898/dating/auth/JoinActivity.kt
+++ app/src/main/java/com/lee989898/dating/auth/JoinActivity.kt
@@ -1,20 +1,44 @@
 package com.lee989898.dating.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.lee989898.dating.MainActivity
 import com.lee989898.dating.R
 
 class JoinActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+
+        auth = Firebase.auth
+
+
 
         val joinBtn = findViewById<Button>(R.id.joinBtn)
         joinBtn.setOnClickListener {
             val email = findViewById<TextInputEditText>(R.id.emailArea)
             val pwd = findViewById<TextInputEditText>(R.id.pwdArea)
+
+            auth.createUserWithEmailAndPassword(email.text.toString(), pwd.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+
+                    } else {
+
+                    }
+                }
 
 
 
