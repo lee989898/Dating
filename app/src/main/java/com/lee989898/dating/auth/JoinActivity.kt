27e0120332db0732ae2815fea9fe7c8f3c602ab1
lee989898 <wnsrud1352@gmail.com>
2022-03-30 00:04:58 +0900

    이메일, 패스워드 받아오기

--- app/src/main/java/com/lee989898/dating/auth/JoinActivity.kt
+++ app/src/main/java/com/lee989898/dating/auth/JoinActivity.kt
@@ -3,6 +3,7 @@ package com.lee989898.dating.auth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 import com.lee989898.dating.R
 
 class JoinActivity : AppCompatActivity() {
@@ -10,6 +11,14 @@ class JoinActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
 
+        val joinBtn = findViewById<Button>(R.id.joinBtn)
+        joinBtn.setOnClickListener {
+            val email = findViewById<TextInputEditText>(R.id.emailArea)
+            val pwd = findViewById<TextInputEditText>(R.id.pwdArea)
+
+
+
+        }
 
 
     }
