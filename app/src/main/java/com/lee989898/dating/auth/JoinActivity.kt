ca0b77713b9b834eb6cfb1239c1531e3f268fb11
lee989898 <wnsrud1352@gmail.com>
2022-03-29 23:58:49 +0900

    feat: 회원가입 디자인

---
+++ app/src/main/java/com/lee989898/dating/auth/JoinActivity.kt
@@ -0,0 +1,16 @@
+package com.lee989898.dating.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.lee989898.dating.R
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+
+
+
+    }
+}
